ed6bff71e039fdb654ac5c7d2f0f9a4440587557
Haydar <shino_aburame@hotmail.com.au>
2018-02-20 19:59:21 +1100

    working on settings

--- ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Boss_Volume.cs
+++ ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Boss_Volume.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Volume_Slider : MonoBehaviour {
+public class Boss_Volume : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Refrence.cs
+++ ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Refrence.cs
@@ -1,2 +1,3 @@
-﻿//https://www.youtube.com/watch?v=IFTjcPvCZaM
-//https://www.youtube.com/watch?v=EA-tBcTxE8M
+﻿//https://www.youtube.com/watch?v=IFTjcPvCZaM - character select
+//https://www.youtube.com/watch?v=EA-tBcTxE8M - settings
+//https://www.youtube.com/watch?v=C65ExBy6WPA, https://www.youtube.com/watch?v=xvpP9U20ioQ - sound
