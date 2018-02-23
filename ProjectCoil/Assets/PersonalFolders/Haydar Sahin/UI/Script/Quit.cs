40900e57dcf4fc3d1ef5641e41df3fba39f4ae42
Haydar <shino_aburame@hotmail.com.au>
2018-02-23 16:44:37 +1100

    added in more stuff to ui
    there is now a quit instead of bouns room. also placed into the scene a bunch of the ui Maddy made

---
+++ ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Quit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour
+{
+    public Button button;
+    private void OnEnable()
+    {
+        if (button != null)
+        {
+            button.onClick.AddListener(QuitGame);
+        }
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
