8aa0687b534f2d893f79079e1ecda70fb8bdf054
Haydar <shino_aburame@hotmail.com.au>
2018-03-20 16:29:41 +1100

    Added buttons for volume

---
+++ ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/VolumeChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeChange : MonoBehaviour
+{
+    public Slider volumeSlider;
+    
+    private Button button;
+
+    [SerializeField] private float volumeAmount;
+    // Use this for initialization
+    public virtual void OnEnable()
+    {
+        button = this.GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(VolumeSliderChanger);
+        }
+    }
+
+    private void VolumeSliderChanger()
+    {
+        volumeSlider.value += volumeAmount;
+        CheckVolume();
+    }
+
+    private void CheckVolume()
+    {
+        if (volumeSlider.value < 0)
+        {
+            volumeSlider.value = 0;
+        }
+        if (volumeSlider.value > 1)
+        {
+            volumeSlider.value = 1;
+        }
+    }
+}
