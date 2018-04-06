d33e2f81b2d2dd74a6eca1b5f53fda2b8e27496b
Haydar <shino_aburame@hotmail.com.au>
2018-04-06 13:32:19 +1000

    working on Audio code

--- ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Refrence.cs
+++ ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Refrence.cs
@@ -3,3 +3,5 @@
 //https://www.youtube.com/watch?v=C65ExBy6WPA - when sfx is in project go to 12:04-13:44, audio listener is SteamVR_ControllerManager return to 15:16
 //https://www.youtube.com/watch?v=xvpP9U20ioQ - when sfx is in project sound
 //https://unity3d.college/2017/06/17/steamvr-laser-pointer-menus/ - vr controller for ui
+
+//Volume_Manager.volumeBoss.PlaySfx(AudioClip,transform.position);
---
+++ ProjectCoil/Assets/PersonalFolders/Haydar Sahin/UI/Script/Volume/Background_Music.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Music : MonoBehaviour
+{
+    public AudioClip battleMusic;
+    public GameObject BattleMusicGameObject;
+    public GameObject relaxMusicGameObject;
+    public AudioClip relaxMusic;
+	// Use this for initialization
+	void Start ()
+	{
+		Volume_Manager.volumeBoss.PlayMusic(battleMusic, 2);
+        //relaxMusicGameObject.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+            Volume_Manager.volumeBoss.PlayMusic(relaxMusic, 2);
+	        //BattleMusicGameObject.SetActive(true);
+        }
+	}
+}
