092195ef72f97edd8df9ad7e8692c1b9da8b281f
Haydar <shino_aburame@hotmail.com.au>
2018-04-18 18:55:36 +1000

    working on fade to black. it is presently to quick

--- ProjectCoil/Assets/PersonalFolders/Haydar Sahin/Script/In_Game/Screen_Change.cs
+++ ProjectCoil/Assets/PersonalFolders/Haydar Sahin/Script/In_Game/Screen_Change.cs
@@ -7,12 +7,25 @@ public class Screen_Change : MonoBehaviour
 {
     private PostProcessingBehaviour myPostProcessingBehaviour;
     private PostProcessingProfile postProcessingProfile;
+    #region Border
+    //border variables
+    private VignetteModel vignetteModel;
+    private VignetteModel.Settings vignetteModelSettings;
     //relevant to border
     public PlayerHealth player;
+    #endregion
+
+    #region Fade
+    private ColorGradingModel colorGradingModel;
+    private ColorGradingModel.Settings colorGradingSettings;
+    public float offsetA;
+    public float fadeSpeed;
+    private Coroutine fadeToBlackCoroutine;
+    #endregion
     // Use this for initialization
     private void OnEnable()
     {
-        #region border veriable set
+        #region gets Prossesser
         myPostProcessingBehaviour = GetComponent<PostProcessingBehaviour>();
         if (myPostProcessingBehaviour == null)
         {
@@ -26,41 +39,75 @@ public class Screen_Change : MonoBehaviour
         #endregion
         //event listener for border
         player.OnHealthChange += VignetteRedChange;
+        vignetteModel = postProcessingProfile.vignette;
+        vignetteModelSettings = vignetteModel.settings;
+        colorGradingModel = postProcessingProfile.colorGrading;
+        colorGradingSettings = colorGradingModel.settings;
+        offsetA = colorGradingSettings.colorWheels.log.offset.a;
     }
 
     #region border functions
     //sets border intensity to match health
     private void VignetteRedChange(float obj)
     {
-        VignetteModel vignetteModel = postProcessingProfile.vignette;
         vignetteModel.enabled = true;
-        VignetteModel.Settings vignetteModelSettings = vignetteModel.settings;
-        vignetteModelSettings.intensity = (((player.health / player.maxHealth)-1)*-1);
+        vignetteModelSettings.intensity = (((player.health / player.maxHealth) - 1) * -1);
         vignetteModel.settings = vignetteModelSettings;
     }
     //resets border intensity to 0
     private void VignetteRedChangeDisabled()
     {
-        VignetteModel vignetteModel = postProcessingProfile.vignette;
-        VignetteModel.Settings vignetteModelSettings = vignetteModel.settings;
         vignetteModelSettings.intensity = 0;
         vignetteModel.settings = vignetteModelSettings;
         vignetteModel.enabled = false;
     }
     #endregion
+
+    #region fade Functions
+    public IEnumerator FadeToBlack()
+    {
+        colorGradingModel.enabled = true;
+        while (offsetA > -1)
+        {
+          offsetA -= fadeSpeed * Time.deltaTime;
+          colorGradingSettings.colorWheels.log.offset.a = offsetA;
+          colorGradingModel.settings = colorGradingSettings;
+        }
+
+        if (offsetA < -1f)
+        {
+            offsetA = -1f;
+        }
+        yield return null;
+    }
+    private void FadeToBlackDisable()
+    {
+        offsetA = 0.0f;
+        if (offsetA > 0)
+        {
+            offsetA = 0;
+        }
+        colorGradingSettings.colorWheels.log.offset.a = offsetA;
+        colorGradingModel.settings = colorGradingSettings;
+        colorGradingModel.enabled = false;
+    }
+    #endregion
     
     // Update is called once per frame //testing code
-	//void Update ()
-	//{
-	//	if(Input.GetKeyDown(KeyCode.F))
-	//	{
-	//	    player.ChangeHealth(-5.0f);
-	//	}
-	//}
-
+    void Update()
+    {
+        colorGradingSettings.colorWheels.log.offset.a = offsetA;
+        colorGradingModel.settings = colorGradingSettings;
+        //offsetA -= 0.02f;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            fadeToBlackCoroutine = StartCoroutine(FadeToBlack());
+        }
+    }
     private void OnDisable()
     {
         VignetteRedChangeDisabled();
+        FadeToBlackDisable();
     }
 
     
