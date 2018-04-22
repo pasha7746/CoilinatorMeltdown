c8b9cf23c2cb483a2955057f22faab5f2be86b78
pasha <pasha.brel@gmail.com>
2018-04-22 22:44:06 +1000

    backup

--- ProjectCoil/Assets/PersonalFolders/Pasha/HeavyAI.cs
+++ ProjectCoil/Assets/PersonalFolders/Pasha/HeavyAI.cs
@@ -1,25 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class HeavyAI : GroundAIBase
 {
     public List<Heavy_Guns> listOfHeavyGuns= new List<Heavy_Guns>();
 
+    public enum Events
+    {
+        Retreat,
+        Attack
+    }
+   
+
+
+
 	// Use this for initialization
 	void Start ()
 	{
 		listOfHeavyGuns.AddRange(GetComponentsInChildren<Heavy_Guns>());
-        PositionToShoot();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
+        listOfHeavyGuns.ForEach((a) =>
+        {
+            a.OnReload += Retreat;
+            a.OnReloadComplete += PositionToShoot;
+            
+        });
+	    PositionToShoot();
+
+    }
+
+
+
 
     public void PositionToShoot()
     {
@@ -35,28 +50,45 @@ public class HeavyAI : GroundAIBase
         }
         else
         {
-            Vector3 pos= new Vector3();
-            pos.y = transform.position.y;
-            pos.x = tempZone.transform.position.x +
-                    Random.Range(-(tempZone.myBoxCollider.size.x / 2), (tempZone.myBoxCollider.size.x / 2));
-            pos.z = tempZone.transform.position.z +
-                    Random.Range(-(tempZone.myBoxCollider.size.z / 2), (tempZone.myBoxCollider.size.z / 2));
-            myAgent.SetDestination(pos);
-            StartCoroutine(WaitForArrivalAtDestination());
+           
+            myAgent.SetDestination(SelectRandomPositionInTheZone(tempZone));
+            StartCoroutine(WaitForArrivalAtDestination(Events.Attack));
         }
         
     }
 
-    public IEnumerator WaitForArrivalAtDestination()
+    public Vector3 SelectRandomPositionInTheZone(Zone tempZone)
     {
+        Vector3 pos = new Vector3();
+        pos.y = transform.position.y;
+        pos.x = tempZone.transform.position.x +
+                Random.Range(-(tempZone.myBoxCollider.size.x / 2), (tempZone.myBoxCollider.size.x / 2));
+        pos.z = tempZone.transform.position.z +
+                Random.Range(-(tempZone.myBoxCollider.size.z / 2), (tempZone.myBoxCollider.size.z / 2));
+        return pos;
+    }
+
+    public IEnumerator WaitForArrivalAtDestination(Events action)
+    {
+        yield return new WaitForSeconds(0.1f);
         while (true)
         {
-            if (myAgent.pathStatus == NavMeshPathStatus.PathComplete)
+         
+           if (myAgent.remainingDistance <= myAgent.stoppingDistance/2)
             {
-                Aim();
+                switch (action)
+                {
+                    case Events.Retreat:
+                        PositionToShoot();
+                        break;
+                    case Events.Attack:
+                        Aim();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("action", action, null);
+                }
                 break;
             }
-
             
         }
         yield return null;
@@ -74,6 +106,25 @@ public class HeavyAI : GroundAIBase
         {
             cGun.Shoot(6);
         }
+
     }
+
+    public void Retreat()
+    {
+        Zone tempZone = myZoneManager.Safe.Find((a) => a.linkedBarricade_1 == masterManager.mySpawnController.currentBarricadeIndex);
+        if (tempZone == null)
+        {
+            tempZone = myZoneManager.Safe.Find((a) => a.linkedBarricade_2 == masterManager.mySpawnController.currentBarricadeIndex);
+            if (tempZone == null)
+            {
+                tempZone = myZoneManager.Safe.Find((a) => a.linkedBarricade_3 == masterManager.mySpawnController.currentBarricadeIndex);
+
+            }
+        }
+        myAgent.SetDestination(SelectRandomPositionInTheZone(tempZone));
+        StartCoroutine(WaitForArrivalAtDestination(Events.Retreat));
+
+    }
+
 
 }
